e121096662e0b5c1c0ad5bf7f10db1224b0b55f9
Daniel Cooney <danielc6275@gmail.com>
2025-08-12 01:00:20 -0400

    COUNTING BUTTONS

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-count = 0
+let count = 0; 
 
-function changeText() {
-  document.getElementById("dynamic-text").innerText = "You clicked the button!";
-}  
+function counter() {
+  count++;
+  document.getElementById("dynamic-text").innerText = `You have clicked this button ${count} times`;
+
+}
+
+function reset() {
+  count = 0;
+  document.getElementById("dynamic-text").innerText = "You have clicked this button 0 times";
+}
